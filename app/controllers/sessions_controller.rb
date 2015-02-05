d925944e61f38e2ce5191cf28f71d54f050fb3dd
Drew Smith <greenwichdrew@gmail.com>
2015-02-04 22:18:16 -0500

    login page goes to list but shows all

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,15 +13,15 @@ class SessionsController < ApplicationController
     teacher = Teacher.find_by_email(params[:email])
     if teacher
       session[:id] = teacher.id
-      redirect_to root_path
+      redirect_to parents_path
     else
-      redirect_to root_path
+      redirect_to parents_path
     end
   end
 
   def destroy
     session[:id] = nil
-    redirect_to root_path, notice: "You have left this page"
+    redirect_to root_path, notice: "Logout to leave this page"
   end
 
 
